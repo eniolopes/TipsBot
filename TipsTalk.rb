6a3177b1536981d9a84850c06d7a3d80d21b6a7b
root <root@darkstar.example.net>
2010-01-10 22:54:51 -0300

    Just corrected some commentaries that wasn`t updated.  Committer: Regis Mesquita <regis@regismesquita.com> 	modified:   README 	modified:   TipsTalk.rb

--- TipsTalk.rb
+++ TipsTalk.rb
@@ -1,15 +1,5 @@
-# Versão 2.1 alpha , TipsTalk
-
-# Software basicamente cria um usuario no gtalk que permite ao usuario postar no twitter, pode ser facilmente expandido para outras funções
-
-# This Software Basically connects to as a google talk user and allow through messages to users post messsages on twitter , can be easily expanded into new functions.
-
-#O script deverá varrer a classe Tipstalk e capturar todas as pessoas, verificar quais já estão adicionadas , mandar convite as que não estão e então iniciar Thread que ficará enviando ao usuario os seus ultimos tweets e twittando para a pessoa, thread deverá ficar identificado em uma hash para o script verificar se a pessoa não já possui uma conexão aberta e se não tiver iniciar uma em loop que verifica a classe tweet talk.
-
-#Tudo acima foi feito na versão 1.0 ,otimizado e transformado para classes na 1.1, bugs corrigidos na 1.2 e convertido pra oAuth na 2.0 e código "limpo"(Ainda falta limpar algumas coisas....) na 2.1 .
-
-#This code is licensed under the terms of GPLv3.
-#Este código esta licenciado sob os termos da GPLv3.
+# Version 0.1 (TipsBot) First Release
+# This Software Basically creates a Google Talk Bot.
 
 require 'rubygems' #Para acessar as gems abaixo
 require 'active_record' # Acesso ao banco de dados.
