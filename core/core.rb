3ba0d03e1c34c1e157ab69a93d8bf266aad1db83
root <root@darkstar.example.net>
2010-01-10 22:47:14 -0300

    First Commit changing internal structure and first lines of code for bot only working.  Committer: Regis Mesquita <regis@regismesquita.com> 	modified:   README 	modified:   TipsTalk.rb 	new file:   core/core.rb 	new file:   externals/functions.rb

---
+++ core/core.rb
@@ -0,0 +1,34 @@
+class Robot
+	attr_accessor :im,:lista_amigos,:lista_proc,:lista_mens
+	def initialize(usuario,senha)
+		@im = Jabber::Simple.new(usuario,senha)
+		@im.accept_subscriptions=true
+		@lista_amigos = Array.new 
+		@lista_proc = Hash.new
+		@lista_mens = Hash.new
+		@methods = Hash.new
+		puts "Main Class Loaded."
+	end
+	#Functions that are going to be used later by the user
+	def add_method(name,function)
+		@methods.store(name,function)
+		puts "Method #{name} added."
+	end
+	#Verify if there's any new message sent by the user.
+	def verificarMensagens
+		puts "Verificando mensagens enviadas."
+		if @im.received_messages?
+			@im.received_messages.each do |mes|
+				#TO-DO: Verify if the command is valid in order to avoid dangerous acts from the user.
+				mes.body.sub(/[^ ]*/){|metodo| 
+					method = @methods[metodo]
+					if method
+						method.call(mes,@im)
+					else
+						@im.deliver(mes.from,"Function does not exist.")
+					end
+				}
+			end
+		end
+	end
+end
---
+++ externals/functions.rb
@@ -0,0 +1,9 @@
+#This file and any other file in this folder should contain external functions
+
+#Example External-function (Echo)
+@robot.add_method("echo",lambda{|mes,im|
+	message = mes.body
+	message['echo']=''
+	im.deliver(mes.from,message)
+})
+
